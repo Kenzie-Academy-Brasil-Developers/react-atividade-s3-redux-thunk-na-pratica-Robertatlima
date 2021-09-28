9149758de7854358981efcbd18cf40ec14f55c6e
Roberta Lima <robertataisaa@gmail.com>
2021-09-28 20:55:10 -0300

    Atividade finalizada

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Display from "./components/Display/index";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Display />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/modules/user/thunk.js
@@ -0,0 +1,13 @@
+import { addComment } from "./action";
+
+export const addComentThunk = (comment) => {
+  return (dispatch, getState) => {
+    if (comment.length !== 0) {
+      const { user } = getState();
+
+      const updateUser = { ...user, comments: [...user.comments, comment] };
+
+      dispatch(addComment(updateUser));
+    }
+  };
+};
